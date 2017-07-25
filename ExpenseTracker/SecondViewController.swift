739d4b8ec295a7c471a6dda1a8436bd551b230f3
吃面包的布拉德 <brad@chimianbaodebuladedeMacBook-Pro.local>
2017-07-25 18:37:15 +1000

    Initial Commit

---
+++ ExpenseTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ExpenseTracker
+//
+//  Created by 吃面包的布拉德 on 2017/7/25.
+//  Copyright © 2017年 吃面包的布拉德. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
